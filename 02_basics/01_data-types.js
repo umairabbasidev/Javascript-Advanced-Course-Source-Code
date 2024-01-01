0a73751064d3d8ee2444dab456c2945a6b63bd21
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-01 19:08:33 +0500

    class two complted concept of data types in js

---
+++ 02_basics/01_data-types.js
@@ -0,0 +1,17 @@
+// Topic Data types in js
+"use strict" // treat all js code as newer version
+
+
+// Primitives data types
+// NN,SS,B,U
+// number=> 2 to power 53;
+// null=> stand alone value  typeof null => object
+// symbol=> to find uniqueness
+// string=>"",''
+// boolean=>true,false
+// bigInt 
+// undefined=> not assign any value yet typeof undefined => undefined
+
+ 
+
+// 
