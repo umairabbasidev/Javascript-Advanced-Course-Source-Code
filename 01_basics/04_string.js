75f13710703083b0573223cbf44e025154259b0d
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-02 09:33:45 +0500

    class 4 complted, concept of string and important methods of string

---
+++ 01_basics/04_string.js
@@ -0,0 +1,97 @@
+// Topic string and some important methods of string
+
+// let str1 = "Hello";
+// let str2 = "World";
+
+// console.log(str1 + " "+ str2)
+
+// let name = "Umair Abbasi";
+// console.log(`my name is ${name.toUpperCase()}`);
+
+// another way to declared string
+
+// let str = new String("Javascript is my faviroute programming language.")
+// console.log(str);
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+// console.log(str.__proto__)
+// console.log(typeof str)
+
+// **********Javascript 20 important strig methods with example***********
+
+// concat(): Combines two or more strings.
+
+// var str1 = "Hello";
+// var str2 = "World";
+// var str3 = "ok";
+// var result = str1.concat(" ", str2," ",str3);
+// console.log(result)
+
+// var str = "Hello";
+// var char = str.charAt(0); // Returns 'H'
+
+
+// var str = "Hello WorldW";
+// var index = str.indexOf("W"); // Returns 6
+
+
+var str = "Hello WorldW";
+var index = str.lastIndexOf("W"); // Returns 11
+
+console.log(index)
+
+
+// var str = "Hello World";
+// var newStr = str.slice(6, 11); // Returns 'World'
+
+
+
+// var str = "hello";
+// var upperStr = str.toUpperCase(); // Returns 'HELLO'
+
+
+// var str = "HELLO";
+// var lowerStr = str.toLowerCase(); // Returns 'hello'
+
+
+// var str = "   Hello   ";
+// var trimmedStr = str.trim(); // Returns 'Hello'
+
+
+// var str = "Hello World";
+// var startsWithHello = str.startsWith("Hello"); // Returns true
+
+
+// var str = "Hello World";
+// var endsWithWorld = str.endsWith("World"); // Returns true
+
+
+
+// var str = "Hello World";
+// var newStr = str.replace("World", "Universe"); // Returns 'Hello Universe'
+
+
+// var str = "Hello,World";
+// var arr = str.split(","); // Returns ['Hello', 'World']
+// console.log(arr)
+
+
+// var str = "Hello ";
+// var repeatedStr = str.repeat(3); // Returns 'Hello Hello Hello '
+
+
+// var str = "Hello";
+// var unicode = str.charCodeAt(0); // Returns 72
+// console.log(unicode)
+
+
+
+
+
+
+
+
+
+
