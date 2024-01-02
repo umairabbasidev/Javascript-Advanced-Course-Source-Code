c52f4172bacddd6a18bee37ec98a41534a74d569
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-02 10:39:44 +0500

    class 5 completed, concept of array and important methods

---
+++ 02_basics/05_array.js
@@ -0,0 +1,43 @@
+// topic arrays
+
+// const myArr = [1,2,3,4,5]
+
+
+// console.log(myArr);
+// console.log(myArr[0])
+// myArr[0]= "Umair"
+// console.log(myArr[0])
+// console.log()
+
+
+// another way to declared array
+
+// const anotherArr = new Array(1,2,3,4,5,"umair",true)
+// console.log(anotherArr)
+
+//  some important methods of array
+
+const arr1 = [1,2,3,4,5]
+console.log(arr1)
+
+const returnValue1 = arr1.push("umair")
+// console.log(arr1) // add values in last
+// console.log(returnValue1) // return new length
+
+const returnValue2 = arr1.pop()
+// console.log(arr1) // remove value in last
+// console.log(returnValue2) // return remove value
+
+const returnValue3 =  arr1.unshift("Hello")
+// console.log(arr1) // add values in first 
+// console.log(returnValue3) // return new length
+
+
+
+
+const returnValue4 =  arr1.shift("Hello")
+// console.log(arr1) // remove value in first
+// console.log(returnValue4) // return remove value
+
+
+
