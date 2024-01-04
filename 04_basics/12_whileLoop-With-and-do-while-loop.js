fa0a4fc7e3c7c952461acf60c8335e35371b5a4a
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-04 10:03:26 +0500

    class 12 completed , concept of for while and do-while  loop

---
+++ 04_basics/12_whileLoop-With-and-do-while-loop.js
@@ -0,0 +1,43 @@
+// Topic White and do-while loop
+
+// while loop with string
+// let str = "Umair Abbasi";
+// let i = 0;
+// while (i<str.length) {
+
+//  console.log(str[i])
+//  i++
+// }
+
+// let i = 0;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+
+// while loop with array 
+// const arr = ["Hello","World","js"]
+// let i =0;
+// while (i<arr.length) {
+//  console.log(arr[i])
+//  i++
+// }
+
+// do while loop with string
+// let str = "Umair"
+// let i = 0 
+// do {
+//  console.log(str[i])
+//  i++
+// } while (i<str.length);
+
+
+// do while loop with array
+// let arr =  ["umair","uzair","asad"]
+// let i = 0 
+// do {
+//  console.log(arr[i])
+//  i++
+// } while (i<arr.length);
