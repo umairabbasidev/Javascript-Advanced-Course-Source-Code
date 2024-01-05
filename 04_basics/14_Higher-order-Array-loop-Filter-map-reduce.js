f36822ac63ed131053107fba32e932b9065e3cc1
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-05 09:34:25 +0500

    starting new concept

---
+++ 04_basics/14_Higher-order-Array-loop-Filter-map-reduce.js
@@ -0,0 +1,53 @@
+// filter,map,reduce
+
+// const coding = ["Js","ruby","java","python","cpp"]
+
+// const values = coding.forEach((item)=>{
+// console.log(item)
+// return item  // foreach value return nahi krta
+// })
+
+// console.log(values)
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const values = myNums.filter( (item)=> item>5)
+// console.log(values)
+
+// filter ka conv=cept ye h hum ose koi bhi condition de gay or jo jo log os condition ko satisfy kre gay wo to hamin retunrn ho kr mil jay gi or jo ni ho gi wo nahi mile gi
+
+// const newNums = [];
+
+// myNums.forEach((item) => {
+//   if (item > 4) {
+//     newNums.push(item);
+//   }
+// });
+
+// console.log(newNums);
+
+const books = [
+  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+];
+
+
+// const userBoxs = books.filter((book)=>{
+// // console.log(item)
+// // return book.genre ==="History"
+// return book.edition>198
+
+// })
+
+// console.log(userBoxs)
+
+
+
+
+
