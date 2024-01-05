2eca0e8fb48bf76d69528f176b6ea00089b9d99d
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-05 09:51:45 +0500

    class 15 completed, concept og global and local scope

---
+++ 05_basics-scope/15_global-and-local-scope.js
@@ -0,0 +1,22 @@
+// Topic : Global and Local scope
+
+// var c =300
+
+let a = 300
+
+if (true) {
+ let a = 10;
+ const b = 20;
+ // var c = 30;
+
+ console.log("inner a", a) // block scope
+}
+
+console.log("outer a",a); // error  // global scope
+// console.log(b); // error
+// console.log(c);
+
+
+// browser mai console mai ja kr jab hum global scope dekhte hn to wo alag h 
+ // or jab hum apne code environemnet mai check krte hn node ki help se to tab global scope alag hota h
+  
